660f9fbb316065e64550b22637e5e9e2c5b4b584
Dylane <tanolamene@gmail.com>
2024-05-18 07:48:26 -0300

    Renommer quelque controle

--- AuteursMangasForm.Designer.cs
+++ AuteursMangasForm.Designer.cs
@@ -28,21 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuteursMangasForm));
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fichierToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.formtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.auteursMangasMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fichiersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouvrirUnePhotosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.policeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ouvrirUnePhotoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.mangasLabel = new System.Windows.Forms.Label();
             this.AuteursLabel = new System.Windows.Forms.Label();
             this.mangas = new System.Windows.Forms.Label();
             this.listeImagesLabel = new System.Windows.Forms.Label();
             this.imageLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.auteursTextBox = new System.Windows.Forms.TextBox();
             this.mangasListBox = new System.Windows.Forms.ListBox();
             this.resultatListBox = new System.Windows.Forms.ListBox();
             this.pairButton = new System.Windows.Forms.Button();
@@ -53,40 +53,54 @@
             this.selectButton = new System.Windows.Forms.Button();
             this.imagesPictureBox = new System.Windows.Forms.PictureBox();
             this.auteursMangasPictureBox = new System.Windows.Forms.PictureBox();
-            this.ouvrirUnePhotosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.policeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
+            this.auteursMangasMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imagesPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.auteursMangasPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // menuStrip1
+            // auteursMangasMenuStrip
             // 
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fichierToolStripMenuItem1,
-            this.formtToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(969, 30);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
+            this.auteursMangasMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.auteursMangasMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichiersToolStripMenuItem,
+            this.formatsToolStripMenuItem});
+            this.auteursMangasMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.auteursMangasMenuStrip.Name = "auteursMangasMenuStrip";
+            this.auteursMangasMenuStrip.Size = new System.Drawing.Size(994, 28);
+            this.auteursMangasMenuStrip.TabIndex = 0;
+            this.auteursMangasMenuStrip.Text = "menuStrip1";
             // 
-            // fichierToolStripMenuItem1
+            // fichiersToolStripMenuItem
             // 
-            this.fichierToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichiersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ouvrirUnePhotosToolStripMenuItem});
-            this.fichierToolStripMenuItem1.Name = "fichierToolStripMenuItem1";
-            this.fichierToolStripMenuItem1.Size = new System.Drawing.Size(66, 26);
-            this.fichierToolStripMenuItem1.Text = "&Fichier";
+            this.fichiersToolStripMenuItem.Name = "fichiersToolStripMenuItem";
+            this.fichiersToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.fichiersToolStripMenuItem.Text = "&Fichier";
             // 
-            // formtToolStripMenuItem
+            // ouvrirUnePhotosToolStripMenuItem
             // 
-            this.formtToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ouvrirUnePhotosToolStripMenuItem.Image = global::AuteursMangas.Properties.Resources._1000_F_468585023_oZ9bWqnreAxAuCa9hhnRx3eMcYLnXdPU;
+            this.ouvrirUnePhotosToolStripMenuItem.Name = "ouvrirUnePhotosToolStripMenuItem";
+            this.ouvrirUnePhotosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ouvrirUnePhotosToolStripMenuItem.Text = "&Ouvrir une photo...";
+            this.ouvrirUnePhotosToolStripMenuItem.Click += new System.EventHandler(this.ouvrirUnePhotosToolStripMenuItem_Click);
+            // 
+            // formatsToolStripMenuItem
+            // 
+            this.formatsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.policeToolStripMenuItem});
-            this.formtToolStripMenuItem.Name = "formtToolStripMenuItem";
-            this.formtToolStripMenuItem.Size = new System.Drawing.Size(70, 26);
-            this.formtToolStripMenuItem.Text = "Forma&t";
+            this.formatsToolStripMenuItem.Name = "formatsToolStripMenuItem";
+            this.formatsToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
+            this.formatsToolStripMenuItem.Text = "Forma&t";
+            // 
+            // policeToolStripMenuItem
+            // 
+            this.policeToolStripMenuItem.Image = global::AuteursMangas.Properties.Resources.letter_A;
+            this.policeToolStripMenuItem.Name = "policeToolStripMenuItem";
+            this.policeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.policeToolStripMenuItem.Text = "&Police...";
+            this.policeToolStripMenuItem.Click += new System.EventHandler(this.policeToolStripMenuItem_Click);
             // 
             // fichierToolStripMenuItem
             // 
@@ -107,12 +121,6 @@
             this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
             this.formatToolStripMenuItem.Size = new System.Drawing.Size(70, 34);
             this.formatToolStripMenuItem.Text = "Forma&t";
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // mangasLabel
             // 
@@ -155,14 +163,14 @@
             this.imageLabel.TabIndex = 9;
             this.imageLabel.Text = "Images:";
             // 
-            // textBox1
+            // auteursTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(18, 368);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(306, 289);
-            this.textBox1.TabIndex = 11;
-            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.auteursTextBox.Location = new System.Drawing.Point(18, 368);
+            this.auteursTextBox.Multiline = true;
+            this.auteursTextBox.Name = "auteursTextBox";
+            this.auteursTextBox.Size = new System.Drawing.Size(306, 289);
+            this.auteursTextBox.TabIndex = 11;
+            this.auteursTextBox.Text = resources.GetString("auteursTextBox.Text");
             // 
             // mangasListBox
             // 
@@ -177,14 +185,14 @@
             // 
             this.resultatListBox.FormattingEnabled = true;
             this.resultatListBox.ItemHeight = 16;
-            this.resultatListBox.Location = new System.Drawing.Point(715, 365);
+            this.resultatListBox.Location = new System.Drawing.Point(727, 368);
             this.resultatListBox.Name = "resultatListBox";
             this.resultatListBox.Size = new System.Drawing.Size(219, 292);
             this.resultatListBox.TabIndex = 13;
             // 
             // pairButton
             // 
-            this.pairButton.Location = new System.Drawing.Point(558, 485);
+            this.pairButton.Location = new System.Drawing.Point(568, 480);
             this.pairButton.Name = "pairButton";
             this.pairButton.Size = new System.Drawing.Size(141, 64);
             this.pairButton.TabIndex = 15;
@@ -194,7 +202,7 @@
             // 
             // impairButton
             // 
-            this.impairButton.Location = new System.Drawing.Point(558, 568);
+            this.impairButton.Location = new System.Drawing.Point(568, 563);
             this.impairButton.Name = "impairButton";
             this.impairButton.Size = new System.Drawing.Size(141, 64);
             this.impairButton.TabIndex = 16;
@@ -218,7 +226,7 @@
             // selectButton
             // 
             this.selectButton.Image = global::AuteursMangas.Properties.Resources.Arrow;
-            this.selectButton.Location = new System.Drawing.Point(555, 402);
+            this.selectButton.Location = new System.Drawing.Point(568, 395);
             this.selectButton.Name = "selectButton";
             this.selectButton.Size = new System.Drawing.Size(141, 64);
             this.selectButton.TabIndex = 14;
@@ -229,7 +237,7 @@
             // 
             this.imagesPictureBox.Location = new System.Drawing.Point(657, 132);
             this.imagesPictureBox.Name = "imagesPictureBox";
-            this.imagesPictureBox.Size = new System.Drawing.Size(277, 180);
+            this.imagesPictureBox.Size = new System.Drawing.Size(289, 180);
             this.imagesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imagesPictureBox.TabIndex = 10;
             this.imagesPictureBox.TabStop = false;
@@ -244,34 +252,18 @@
             this.auteursMangasPictureBox.TabIndex = 3;
             this.auteursMangasPictureBox.TabStop = false;
             // 
-            // ouvrirUnePhotosToolStripMenuItem
-            // 
-            this.ouvrirUnePhotosToolStripMenuItem.Image = global::AuteursMangas.Properties.Resources._1000_F_468585023_oZ9bWqnreAxAuCa9hhnRx3eMcYLnXdPU;
-            this.ouvrirUnePhotosToolStripMenuItem.Name = "ouvrirUnePhotosToolStripMenuItem";
-            this.ouvrirUnePhotosToolStripMenuItem.Size = new System.Drawing.Size(213, 26);
-            this.ouvrirUnePhotosToolStripMenuItem.Text = "&Ouvrir une photo...";
-            this.ouvrirUnePhotosToolStripMenuItem.Click += new System.EventHandler(this.ouvrirUnePhotosToolStripMenuItem_Click);
-            // 
-            // policeToolStripMenuItem
-            // 
-            this.policeToolStripMenuItem.Image = global::AuteursMangas.Properties.Resources.letter_A;
-            this.policeToolStripMenuItem.Name = "policeToolStripMenuItem";
-            this.policeToolStripMenuItem.Size = new System.Drawing.Size(140, 26);
-            this.policeToolStripMenuItem.Text = "&Police...";
-            this.policeToolStripMenuItem.Click += new System.EventHandler(this.policeToolStripMenuItem_Click);
-            // 
             // AuteursMangasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(969, 706);
+            this.ClientSize = new System.Drawing.Size(994, 702);
             this.Controls.Add(this.listeImagesListBox);
             this.Controls.Add(this.impairButton);
             this.Controls.Add(this.pairButton);
             this.Controls.Add(this.selectButton);
             this.Controls.Add(this.resultatListBox);
             this.Controls.Add(this.mangasListBox);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.auteursTextBox);
             this.Controls.Add(this.imagesPictureBox);
             this.Controls.Add(this.imageLabel);
             this.Controls.Add(this.listeImagesLabel);
@@ -279,14 +271,14 @@
             this.Controls.Add(this.AuteursLabel);
             this.Controls.Add(this.auteursMangasPictureBox);
             this.Controls.Add(this.mangasLabel);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.Controls.Add(this.auteursMangasMenuStrip);
+            this.MainMenuStrip = this.auteursMangasMenuStrip;
             this.Name = "AuteursMangasForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auteurs et Mangas";
             this.Load += new System.EventHandler(this.AuteursMangasForm_Load);
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.auteursMangasMenuStrip.ResumeLayout(false);
+            this.auteursMangasMenuStrip.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.imagesPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.auteursMangasPictureBox)).EndInit();
             this.ResumeLayout(false);
@@ -296,11 +288,10 @@
 
         #endregion
 
-        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip auteursMangasMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ouvrirUnePhotoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label mangasLabel;
         private System.Windows.Forms.PictureBox auteursMangasPictureBox;
         private System.Windows.Forms.Label AuteursLabel;
@@ -308,16 +299,16 @@
         private System.Windows.Forms.Label listeImagesLabel;
         private System.Windows.Forms.Label imageLabel;
         private System.Windows.Forms.PictureBox imagesPictureBox;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox auteursTextBox;
         private System.Windows.Forms.ListBox mangasListBox;
         private System.Windows.Forms.ListBox resultatListBox;
         private System.Windows.Forms.Button selectButton;
         private System.Windows.Forms.Button pairButton;
         private System.Windows.Forms.Button impairButton;
         private System.Windows.Forms.ListBox listeImagesListBox;
-        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem fichiersToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ouvrirUnePhotosToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem formtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem policeToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog auteursMangasOpenFileDialog;
         private System.Windows.Forms.FontDialog auteursMangasFontDialog;
